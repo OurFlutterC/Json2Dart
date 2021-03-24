3ba5acaa464175f72c99939fab4aeb9bb0786470
m97chahboun <m97.chahboun@gmail.com>
2021-03-24 10:42:01 +0100

    emove python tool & optimize dart tool

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 # Json2Dart
- Json2Dart tool to convert Json data to dart model with data directly or path of json file or link API
+ Json2Dart tool to convert Json data to dart model with data directly or path of json file 
 # HOW TO USE
-run Json2Dart.exe 
+type in terminal dart Json2Dart.dart 
 past json data directly like : {"country":"World","cases":4525390,"todayCases":3364,"deaths":303371,"todayDeaths":289,"recovered":1703775,"active":2518244,"critical":45560,"casesPerOneMillion":581,"deathsPerOneMillion":38,"totalTests":0,"testsPerOneMillion":0}
 or
 Path of json file like jsonData.json
-or 
-link Api : https://coronavirus-19-api.herokuapp.com/countries
 click enter 
 and write class name and click enter
 the tool will create file and create model...
 
-# Download exe file
-[Click here](https://www.mediafire.com/file/t847rwvfhiad365/json2dart.zip/file)
+# Website 
+- [Click here](json2dart.web.app/)
 
 Not Forget support *****
--- json2Dart.dart
+++ json2Dart.dart
@@ -1,50 +1,181 @@
 import 'dart:convert';
 import 'dart:io';
 
-void json2Dart(String inputUser, className) {
+Map mdl = {};
+Map json2Dart(String inputUser, String className) {
+  className = className.isEmpty
+      ? "MyModel"
+      : className.substring(0, 1).toUpperCase() + className.substring(1);
+
+  inputUser = inputUser.isEmpty ? '{"mcPackage":"mc"}' : inputUser;
   try {
-    List jsonInputUser = json.decode(inputUser);
+    var jsonInputUser = json.decode(inputUser.trim());
     String data = "";
     String parameters = "";
     String fromVar = "";
     String toVar = "";
-    for (Map i in jsonInputUser) {
-      for (String u in i.keys) {
-        String t = i[u].runtimeType.toString();
-        data += """ $t $u;\n""";
-        parameters += """  this.$u,\n""";
-        fromVar += "  $u = json['$u'] ?? $u;\n";
-        toVar += "  data['$u'] = this.$u;\n";
+    String initial = "";
+    String tj = "";
+    String multi = "";
+    if (jsonInputUser is List) {
+      initial = "\n\n  multi = multi ?? [];\n";
+      data += " List multi;\n";
+      multi = """\n\nvoid setMulti(List d) {
+        List r = d.map((e) {
+          $className m = $className();
+          m.fromJson(e);
+          return m;
+            }).toList();
+            multi = r;
+          }\n""";
+
+      for (Map i in jsonInputUser) {
+        for (String u in i.keys) {
+          if (i[u] is String) {
+            data += """ String $u;\n""";
+            fromVar += "  $u = json['$u'] ?? $u;\n";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+          } else if (i[u] is num && i[u].toString().contains(".")) {
+            data += """ double $u;\n""";
+            fromVar += "  $u = json['$u'] ?? $u;\n";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+          } else if (i[u] is int) {
+            data += """ int $u;\n""";
+            fromVar += "  $u = json['$u'] ?? $u;\n";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+          } else if (i[u] is List) {
+            if (i[u][0] is List) {
+              String mdl = u.substring(0, 1).toUpperCase() + u.substring(1);
+              json2Dart(json.encode(i[u]), mdl);
+              data += """ $mdl $u;\n""";
+              initial += "  $u ??= $mdl();\n";
+              fromVar += "  $u.fromJson(json['$u'] ?? $u.toJson());\n";
+              tj = ".toJson()";
+              parameters += """  this.$u,\n""";
+              toVar += "  data['$u'] = this.$u$tj;\n";
+              tj = "";
+            } else if (i[u][0] is Map) {
+              String mdl = u.substring(0, 1).toUpperCase() + u.substring(1);
+              json2Dart(json.encode(i[u]), mdl);
+              data += """ $mdl $u;\n""";
+              initial += "  $u ??= $mdl();\n";
+              fromVar += "  $u.fromJson(json['$u'] ?? $u.toJson());\n";
+              tj = ".toJson()";
+              parameters += """  this.$u,\n""";
+              toVar += "  data['$u'] = this.$u$tj;\n";
+              tj = "";
+            } else {
+              data += """ List $u;\n""";
+              fromVar += "  $u = json['$u'] ?? $u;\n";
+              parameters += """  this.$u,\n""";
+              toVar += "  data['$u'] = this.$u$tj;\n";
+            }
+          } else if (i[u] is Map) {
+            String mdl = u.substring(0, 1).toUpperCase() + u.substring(1);
+            json2Dart(json.encode(i[u]), mdl);
+            data += """ $mdl $u;\n""";
+            initial += "  $u ??= $mdl();\n";
+            fromVar += "  $u.fromJson(json['$u'] ?? $u.toJson());\n";
+            tj = ".toJson()";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+            tj = "";
+          } else {
+            print("\n>> See this type is not their .\n");
+          }
+        }
+        break;
       }
-      break;
+    } else {
+      for (String u in jsonInputUser.keys) {
+        if (jsonInputUser[u] is String) {
+          data += """ String $u;\n""";
+          fromVar += "  $u = json['$u'] ?? $u;\n";
+          parameters += """  this.$u,\n""";
+          toVar += "  data['$u'] = this.$u$tj;\n";
+        } else if (jsonInputUser[u] is num &&
+            jsonInputUser[u].toString().contains(".")) {
+          data += """ double $u;\n""";
+          fromVar += "  $u = json['$u'] ?? $u;\n";
+          parameters += """  this.$u,\n""";
+          toVar += "  data['$u'] = this.$u$tj;\n";
+        } else if (jsonInputUser[u] is int) {
+          data += """ int $u;\n""";
+          fromVar += "  $u = json['$u'] ?? $u;\n";
+          parameters += """  this.$u,\n""";
+          toVar += "  data['$u'] = this.$u$tj;\n";
+        } else if (jsonInputUser[u] is List) {
+          if (jsonInputUser[u][0] is List) {
+            String mdl = u.substring(0, 1).toUpperCase() + u.substring(1);
+            json2Dart(json.encode(jsonInputUser[u]), mdl);
+            data += """ $mdl $u;\n""";
+            initial += "  $u ??= $mdl();\n";
+            fromVar += "  $u.fromJson(json['$u'] ?? $u.toJson());\n";
+            tj = ".toJson()";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+            tj = "";
+          } else if (jsonInputUser[u][0] is Map) {
+            String mdl = u.substring(0, 1).toUpperCase() + u.substring(1);
+            json2Dart(json.encode(jsonInputUser[u]), mdl);
+            data += """ $mdl $u;\n""";
+            initial += "  $u ??= $mdl();\n";
+            fromVar += "  $u.fromJson(json['$u'] ?? $u.toJson());\n";
+            tj = ".toJson()";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+            tj = "";
+          } else {
+            data += """ List $u;\n""";
+            fromVar += "  $u = json['$u'] ?? $u;\n";
+            parameters += """  this.$u,\n""";
+            toVar += "  data['$u'] = this.$u$tj;\n";
+          }
+        } else if (jsonInputUser[u] is Map) {
+          List a = [];
+          a.add(jsonInputUser[u]);
+          String mdl = u.substring(0, 1).toUpperCase() + u.substring(1);
+          json2Dart(json.encode(a), mdl);
+          data += """ $mdl $u;\n""";
+          initial += "  $u ??= $mdl();\n";
+          fromVar += "  $u.fromJson(json['$u'] ?? $u.toJson());\n";
+          tj = ".toJson()";
+          parameters += """  this.$u,\n""";
+          toVar += "  data['$u'] = this.$u$tj;\n";
+          tj = "";
+        } else {
+          print("\n>> Invalid .\n");
+        }
+      }
     }
-    data += " List multi = [];\n";
+
     fromVar += "  return super.fromJson(json);\n";
-    String multi = """\n\nvoid setMulti(List d) {
-    List r = d.map((e) {
-      $className m = $className();
-      m.fromJson(e);
-      return m;
-    }).toList();
-    multi = r;
-  }\n""";
+
     String toJson =
         " Map<String, dynamic> toJson() {\n final Map<String, dynamic> data = new Map<String, dynamic>();";
     String fromJson = " fromJson(Map<String, dynamic> json) {";
-    String class_ = "class $className extends McModel{\n";
+    String headClass = "class $className extends McModel{\n";
     String constractor = " $className({";
-    String model = class_ +
+    String init = initial.isNotEmpty ? "}" : ";";
+    String it = initial.isNotEmpty ? "{\n" : "";
+    String model = headClass +
         data +
         "\n" +
         constractor +
         "\n" +
         parameters +
-        " });\n" +
+        " })" +
+        it +
+        initial +
+        init +
         "\n" +
         fromJson +
         "\n" +
         fromVar +
-        " }" +
+        " }\n" +
         "\n\n" +
         toJson +
         "\n" +
@@ -55,33 +186,39 @@ void json2Dart(String inputUser, className) {
         multi +
         "\n" +
         "}";
-    File("${className}Model.dart").writeAsString(model);
-    print(model);
+
+    mdl[className] = model;
+
+    return mdl;
   } catch (e) {
     print("[-] $e");
+    return {"error": "[-Error] $e"};
   }
 }
 
 //String getData(link) {}
 
 void main() {
-  print(" [+] Your Json data or path file or link Api: ");
-  String data = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+  print(" [+] Your Json data or path json file: ");
+  String? data = stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
   print(" [+] Your dart class name (default name MyModel):");
-  String className = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
-  if (className.length == 0) {
-    className = "MyModel";
-  }
+  String? className =
+      stdin.readLineSync(encoding: Encoding.getByName('utf-8')!);
 
   try {
     //  if (data.substring(4) == "http"){
     //    Map api = getData(data);
     //  }else{
-    if (data.contains(".json")) {
+    if (data!.contains(".json")) {
       data = File(data).readAsStringSync();
     }
+    json2Dart(data, className!);
 
-    json2Dart(data, className);
+    
+    for (String item in mdl.keys) {
+      File("${item}Model.dart").writeAsString(
+          mdl[item.substring(0, 1).toUpperCase() + item.substring(1)]);
+    }
   } catch (e) {
     print("[-] $e");
   }
